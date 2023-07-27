3fdef13242db07a5e0131f10065bea5ebe0d2a1e
zhujun <1138586660@qq.com>
2023-07-27 20:52:52 +0800

    测试接口增加返回日期

--- src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
+++ src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
@@ -1,10 +1,12 @@
 package com.atguigu.paymentdemo.controller;
 
+import com.atguigu.paymentdemo.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.Date;
 
 @Api(tags = "商品管理")
 @RestController
@@ -13,8 +15,8 @@ public class ProductController {
 
     @ApiOperation("测试接口")
     @GetMapping("/test")
-    public String test() {
-        return "hello";
+    public R test() {
+        return R.ok().data("message", "hello").data("now", new Date());
     }
 }
 
