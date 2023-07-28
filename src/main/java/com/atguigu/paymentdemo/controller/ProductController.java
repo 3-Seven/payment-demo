7f6e321a89bcdd3f3d57937c6cf298ef7954511c
zhujun <1138586660@qq.com>
2023-07-28 15:44:57 +0800

    接口开放前端跨域访问

--- src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
+++ src/main/java/com/atguigu/paymentdemo/controller/ProductController.java
@@ -5,6 +5,7 @@ import com.atguigu.paymentdemo.service.ProductService;
 import com.atguigu.paymentdemo.vo.R;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import javax.annotation.Resource;
 import java.util.Date;
 import java.util.List;
 
+@CrossOrigin //开放前端的跨域访问
 @Api(tags = "商品管理")
 @RestController
 @RequestMapping("/api/product")
